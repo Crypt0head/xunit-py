112cc75d11dbd16c0b8e40a35bbc5ecf22c37145
Crypt0head <zelas.money@yandex.ru>
2022-07-25 19:30:59 +0300

    Added Parant class TestCase. Added special test class TestClassTest inharited from TestCase

--- test.py
+++ test.py
@@ -1,11 +1,26 @@
-class WasRun:
+class TestCase:
+    def __init__(self, name):
+        self.name = name
+
+    def run(self):
+        method = getattr(self, self.name)
+        method()
+
+class WasRun(TestCase):
     def __init__(self, name):
         self.wasRun = None
+        TestCase.__init__(self, name)
+
     def testMethod(self):
         self.wasRun = 1
 
-#TEST CASE
-test = WasRun("testMethod")
-print(test.wasRun)
-test.run()
-print(test.wasRun)
+# TEST CASE
+class TestCaseTest(TestCase):
+    def testRunning(self):
+        test = WasRun("testMethod")
+        assert(not test.wasRun)
+        test.run()
+        assert(test.wasRun)
+        print('[' + self.name + '] passed OK!')
+
+TestCaseTest("testRunning").run()
