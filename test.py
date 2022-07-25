1480948e104aba2ae401eb05412a05fbd8ec41bb
Crypt0head <zelas.money@yandex.ru>
2022-07-25 18:14:16 +0300

    Init commit

---
+++ test.py
@@ -0,0 +1,11 @@
+class WasRun:
+    def __init__(self, name):
+        self.wasRun = None
+    def testMethod(self):
+        self.wasRun = 1
+
+#TEST CASE
+test = WasRun("testMethod")
+print(test.wasRun)
+test.run()
+print(test.wasRun)
