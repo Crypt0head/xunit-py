513a105087fb3ecd476cd019ba9c0ce54299ad76
Crypt0head <zelas.money@yandex.ru>
2022-07-27 07:47:28 +0300

    added tearDown method and refactored boolean flags

--- test.py
+++ test.py
@@ -5,36 +5,35 @@ class TestCase:
     def setUp(self):
         pass
 
+    def tearDown(self):
+        pass
+
     def run(self):
         self.setUp()
         method = getattr(self, self.name)
         method()
+        self.tearDown()
+
 
 class WasRun(TestCase):
     def __init__(self, name):
         TestCase.__init__(self, name)
 
     def testMethod(self):
-        self.wasRun = 1
+        self.log = self.log + "testMethod "
 
     def setUp(self):
-        self.wasRun = None
-        self.wasSetUp = 1
+        self.log = "setUp "
+
+    def tearDown(self):
+        self.log = self.log + "tearDown "
 
 # TEST CASE
 class TestCaseTest(TestCase):
-    def setUp(self):
-        self.test = WasRun("testMethod")
-
-    def testRunning(self):
-        self.test.run()
-        assert(self.test.wasRun)
+    def testTemplateMethod(self):
+        test = WasRun("testMethod")
+        test.run()
+        assert("setUp testMethod tearDown " == test.log)
         print('[' + self.name + '] passed OK!')
 
-    def testSetUp(self):
-        self.test.run()
-        assert(self.test.setUp)
-        print('[' + self.name + '] passed OK!')
-
-TestCaseTest("testRunning").run()
-TestCaseTest("testSetUp").run()
+TestCaseTest("testTemplateMethod").run()
